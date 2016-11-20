24a016e4612010971cdaf3bb8e6270e91058122b
Nathan Binks <FoulPlay@users.noreply.github.com>
2016-11-20 11:23:01 +0000

    Updated KillerSpawner to 1.1.0 TEST
    Version 1.1.0 TEST

--- AI-Scripts/Killerontheloose.cs
+++ AI-Scripts/Killerontheloose.cs
@@ -2,7 +2,7 @@
 using System.Windows.Forms;
 using GTA;
 
-//Created by Nathan Binks (Foul Play), Version 1.0.0 TEST
+//Created by Nathan Binks (Foul Play), Version 1.1.0 TEST
 
 namespace KillerSpawner
 {
@@ -15,7 +15,7 @@ namespace KillerSpawner
         {
             this.KeyUp += onKeyUp;
             this.Tick += onTick;
-            Interval = 1000;
+            Interval = 1;
         }
 
         private void onKeyUp(object sender, GTA.KeyEventArgs e)
@@ -24,6 +24,14 @@ namespace KillerSpawner
             {
                 CreateKiller(); //Creates the killer
             }
+            if (e.Shift && e.Key == Keys.D9)
+            {
+                if (killer != null && Exists(killer))
+                {
+                    killer.NoLongerNeeded();
+                    killer = null;
+                }
+            }
         }
 
         private void onTick(object sender, EventArgs e)
@@ -31,15 +39,6 @@ namespace KillerSpawner
             //If the ped exsist
             if (killer != null && Exists(killer))
             {
-                //If the ped is not in combat
-                if (!killer.isInCombat)
-                {
-                    killer.Task.WanderAround(); //Just wander around until in combat
-                }
-                else
-                {
-                    killer.StartKillingSpree(false); //Kill everyone muhahaha but me
-                }
                 //if the killer is dead then make it no longer needed and null
                 if (killer.isDead)
                 {
@@ -60,8 +59,11 @@ namespace KillerSpawner
             killer.CanSwitchWeapons = true; //Allow the ped to switch weapons
             killer.WantedByPolice = true; //Make the police attack the ped
             killer.SetPathfinding(true, true, true); //Allows the ped to climb over objects, use ladders and drop from heights
-            killer.Weapons.AnyHandgun.Ammo = 50; //Give a random Handgun
-            killer.Weapons.AnyAssaultRifle.Ammo = 9999; //Give a random Assault Rifle
+            killer.AlwaysDiesOnLowHealth = true; //Always die when having low health
+            killer.Weapons.DesertEagle.Ammo = 50;//Give them a weapon
+            killer.Weapons.AssaultRifle_AK47.Ammo = 100;
+           // killer.Weapons.Select(killer.Weapons.DesertEagle); //Select the weapon
+            killer.StartKillingSpree(false); //Start the killing spree
         }
     }
 }
